ba944b7731b6458f72e974290b3af667a8107e15
sunhuajie02 <sunhuajie02@58.com>
2023-04-14 17:11:46 +0800

    测试 asset读取json文件 工具类

---
+++ app/src/main/java/senduo/com/senduohttp/JsonAssetsUtil.java
@@ -0,0 +1,36 @@
+package senduo.com.senduohttp;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author :      SunHuaJie
+ * @date :        2023/4/13-15:56
+ * @email :       1214495199@qq.com
+ * @description :
+ */
+public class JsonAssetsUtil {
+    public static String getJson(String fileName, Context context) {
+        //将json数据变成字符串
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            //获取assets资源管理器
+            AssetManager assetManager = context.getAssets();
+            //通过管理器打开文件并读取
+            BufferedReader bf = new BufferedReader(new InputStreamReader(
+                    assetManager.open(fileName)));
+            String line;
+            while ((line = bf.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+
+}
